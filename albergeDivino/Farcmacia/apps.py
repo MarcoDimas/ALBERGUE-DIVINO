ff127b749020e46b13026d7efa5382c9b1d5fc7f
MarcoDimas <marcodimas1234@gmail.com>
2024-06-24 18:16:24 -0600

    renombrar app

--- albergeDivino/Farcmacia/apps.py
+++ albergeDivino/Farcmacia/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ContenidoConfig(AppConfig):
+class FarcmaciaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'contenido'
+    name = 'Farcmacia'
