fc8f8ccbd0ca0f686b15e53fe9018c5d19c38c39
MarcoDimas <marcodimas1234@gmail.com>
2024-07-04 14:48:32 -0600

    vista para recuperar datos de la bd

--- albergeDivino/Farmacia/views.py
+++ albergeDivino/Farmacia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Medicamento
 
 def paginaPrincipal(request):
     return render(request, 'Farmacia/principal.html')
@@ -11,4 +11,5 @@ def salidaMedicamentos(request):
      return render(request, 'Farmacia/salidaMedicamentos.html')
 
 def verMedicamentos(request):
-     return render(request, 'Farmacia/verMedicamentos.html')
+    medicamento =Medicamento.objects.all()
+    return render(request, "Farmacia/verMedicamentos.html", {'MD':medicamento})
